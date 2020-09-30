8a4978548ed6bb767ffc9d79f6b1e6d419235385
sahiabhi <abhikatakam96@gmail.com>
2020-09-30 12:09:35 +0530

    Added AppTest

---
+++ MavenApp2/src/test/java/com/cg/projectTest/AppTest.java
@@ -0,0 +1,25 @@
+package com.cg.projectTest;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
+	}
+
+
